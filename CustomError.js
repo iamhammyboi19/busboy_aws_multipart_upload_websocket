0333048d438048680ca415809c1f68594a637827
@iamhammyboi19 <88974019+iamhammyboi19@users.noreply.github.com>
2025-02-07 22:27:11 +0100

    Create CustomError.js

---
+++ CustomError.js
@@ -0,0 +1,16 @@
+module.exports = class CustomError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = true;
+    this.status =
+      String(statusCode).startsWith("4") || String(statusCode).startsWith("3")
+        ? "error"
+        : "fail";
+
+    // this -> gives error.stack property
+    // this.constructor -> makes sure error stack trace wont involve CustomError class but training starts where the error itself occur eg error at /Users/user/Desktop/USER_RES_ORDER_PRODS_CARTS/middlewares/send_email_token.js
+    // instead of starting from /Users/user/Desktop/USER_RES_ORDER_PRODS_CARTS/utils/CustomError.js
+    Error.captureStackTrace(this, this.constructor);
+  }
+};
